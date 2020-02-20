a6ba64bea4c62407ffb099aadc7249d110627201
udaykalyan97 <57364428+udaykalyan97@users.noreply.github.com>
2020-02-20 11:11:33 -0500

    Update contourplot.m

--- contourplot.m
+++ contourplot.m
@@ -1,7 +1,10 @@
 load('Coordinates.mat');
-for i=4:83
-data = IndexAlpha(2:21,i);
-data = cell2mat(data);
+D=readtable('S3_nue.xlsx');
+
+figure('Name','S3_nuetral - Alpha','NumberTitle','off');
+for i=3:26
+data = table2array(D(1:222,i));
+%data = cell2mat(data);
 % Create polar data
 [t,r] = meshgrid((0:1:360)*pi/180,[0 50 100]);
 
@@ -11,23 +14,37 @@ F = scatteredInterpolant(XCoord',YCoord',data);
 Z = F(X,Y);
 
 % Plot a graph.
-figure
+
+%figure;
+subplot(6,4,i-2);
 contourf(X,Y,Z,100,'LineColor', 'none');
 daspect([1 1 2])
 
-% colormap
-c = hsv;
-c = c(1:64,:);
-colormap jet;
 
-colorbar('southoutside');
+% colormap
+
+c = jet;
+a = c(1:64,:);
+colormap jet;
+%set(gca, 'clim', [min(Ci(:)) max(Ci(:))]);
+
+set(gca, 'clim', [0,4.54]);
 
 % scatter overlay
 hold on
 
 sz = 30;
 
-scatter(XCoord,YCoord,sz,'fill','k');
+%scatter(XCoord,YCoord,sz,'fill','k');
+m=(i-3)*50;
 
-title('S14 - NEGATIVE, alpha')
+title(['t=', num2str(m),'ms']);
+ %to save files in PNG 
+ %temp=['fig',num2str(i),'.png']; 
+ %saveas(gca,temp); 
+
 end
+%colorbar('eastoutside');
+%colorbar('Position',[0.911328125,0.106481481481481,0.0177734375,0.814043209876543])
+colorbar('Position',[0.9,0.11,0.02,0.813])
+
